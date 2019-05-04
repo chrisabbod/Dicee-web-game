17378a2d8dc79045ad1d5264aa8c456432ae9fdc
Chris Abbod <chrisabbod@Chris-MacBook-Pro.local>
2019-05-03 21:44:26 -0400

    Refactor index.js

--- index.js
+++ index.js
@@ -1,51 +1,10 @@
 var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+
 var randomNumber2 = Math.floor((Math.random() * 6) + 1);
-
-switch (randomNumber1) {
-    case 1:
-        document.querySelector(".img1").setAttribute("src", "images/dice1.png");
-        break;
-    case 2:
-        document.querySelector(".img1").setAttribute("src", "images/dice2.png");
-        break;
-    case 3:
-        document.querySelector(".img1").setAttribute("src", "images/dice3.png");
-        break;
-    case 4:
-        document.querySelector(".img1").setAttribute("src", "images/dice4.png");
-        break;
-    case 5:
-        document.querySelector(".img1").setAttribute("src", "images/dice5.png");
-        break;
-    case 6:
-        document.querySelector(".img1").setAttribute("src", "images/dice6.png");
-        break;
-    default:
-        console.log("Nothing Chosen");
-}
-
-switch (randomNumber2) {
-    case 1:
-        document.querySelector(".img2").setAttribute("src", "images/dice1.png");
-        break;
-    case 2:
-        document.querySelector(".img2").setAttribute("src", "images/dice2.png");
-        break;
-    case 3:
-        document.querySelector(".img2").setAttribute("src", "images/dice3.png");
-        break;
-    case 4:
-        document.querySelector(".img2").setAttribute("src", "images/dice4.png");
-        break;
-    case 5:
-        document.querySelector(".img2").setAttribute("src", "images/dice5.png");
-        break;
-    case 6:
-        document.querySelector(".img2").setAttribute("src", "images/dice6.png");
-        break;
-    default:
-        console.log("Nothing Chosen");
-}
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
     document.querySelector(".container h1").innerHTML = "🚩 Player1 Wins!";
